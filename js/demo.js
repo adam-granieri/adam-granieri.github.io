0f8de15d80b2c54b59db8070f2ee2ca50485a1d3
Adam Granieri <adam.granieri@gmail.com>
2019-07-29 21:13:35 -0400

    updated demo

--- js/demo.js
+++ js/demo.js
@@ -99,7 +99,7 @@ function createScene() {
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   renderer.setSize(sceneWidth, sceneHeight);
-  dom = document.getElementById("container");
+  dom = document.getElementById("AdamContainer");
   dom.appendChild(renderer.domElement);
 
   // Individual Function Calls for geometries, animations and lighting
@@ -216,11 +216,11 @@ function generateLighting() {
   sun = new THREE.DirectionalLight(0xcdc1c5, 0.9);
   sun.position.set(12, 6, -7);
   sun.castShadow = true;
+  scene.add(sun);
   sun.shadow.mapSize.width = 256;
   sun.shadow.mapSize.height = 256;
   sun.shadow.camera.near = 0.5;
   sun.shadow.camera.far = 50;
-  scene.add(sun);
 }
 
 function generateExplosion() {
